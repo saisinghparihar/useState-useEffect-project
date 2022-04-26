8359754fda0a1a76538afebf395bca608a7f38f7
saisinghparihar <saisinghparihar626@gmail.com>
2022-04-26 22:26:40 +0530

    final project

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import searchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+
+const API = "http://www.omdbapi.com/?i=tt3896198&apikey=ba6eb971";
+
+export default function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchMovie = async (title) => {
+    await fetch(`${API}&s=${title}`).then((response) =>
+      response.json().then((data) => setMovies(data.Search))
+    );
+  };
+  useEffect(() => {
+    searchMovie(searchTerm);
+  }, [searchTerm]);
+
+  return (
+    <div className="app">
+      <h1>Sasta Netflix</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+        <img
+          src={searchIcon}
+          alt="search-icon"
+          onClick={() => {
+            searchMovie(searchTerm);
+          }}
+        />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.imdbID} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
+
+//ba6eb971 my api key
